8089538580405d28eb56d1f5646929dc5fe7d68d
Chaitanya K Godsay <ckgodsay@A001565.local>
2020-08-27 17:50:47 -0700

    Added notes api, dynamodb integration, s3 integration

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,13 @@ const awsmobile = {
     "aws_cognito_region": "us-west-2",
     "aws_user_pools_id": "us-west-2_VZKqFXIps",
     "aws_user_pools_web_client_id": "79fdiq3v94jrv24dfuh100p7f4",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://dcclywyarrdepcwvfzjpawvn2u.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-yglz7iw6x5cfliqqtpjdcds4te",
+    "aws_user_files_s3_bucket": "first-s3-bucket154428-dev",
+    "aws_user_files_s3_bucket_region": "us-west-2"
 };
 
 
